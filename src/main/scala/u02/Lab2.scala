06944da87b5dd5a396cb75555a510dd87bb08c15
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2025-03-13 18:15:46 +0100

    Implemented task 4

--- src/main/scala/u02/Lab2.scala
+++ src/main/scala/u02/Lab2.scala
@@ -46,14 +46,40 @@ object Lab2 extends App:
   val notZero = genericNeg(isZero)
   val notEmptyWithGenerics = genericNeg(empty)
 
-  // Test per la funzione notZero
   println(s"Test notZero function passing 2 as input. Expected: true, Result: ${notZero(2)}")
   println(s"Test notZero function passing 0 as input. Expected: false, Result: ${notZero(0)}")
   println(s"Test notZero function passing '(notZero(2) && !notZero(0))' as input. Expected: true, Result: ${(notZero(2) && !notZero(0))}")
 
-  // Test per la funzione notEmptyWithGenerics
   println(s"Test notEmptyWithGenerics function passing 'foo' as input. Expected: true, Result: ${notEmptyWithGenerics("foo")}")
   println(s"Test notEmptyWithGenerics function passing an empty string as input. Expected: false, Result: ${notEmptyWithGenerics("")}")
   println(s"Test notEmptyWithGenerics function passing '(notEmptyWithGenerics(\"foo\") && !notEmptyWithGenerics(\"\"))' as input. Expected: true, Result: ${(notEmptyWithGenerics("foo") && !notEmptyWithGenerics(""))}")
+  println()
+
+  println("Task 4")
+  val p1: Int => Int => Int => Boolean = x => y => z => x<=y && y==z
+  val p2: (Int, Int, Int) => Boolean = (x,y,z) => x<=y && y==z
+  def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y == z
+  def p4(x: Int, y: Int, z: Int): Boolean = x <= y && y == z
+
+  println(s"Test p1 function passing (1, 2, 2) as input. Expected: true, Result: ${p1(1)(2)(2)}")
+  println(s"Test p1 function passing (3, 2, 2) as input. Expected: false, Result: ${p1(3)(2)(2)}")
+
+  println(s"Test p2 function passing (1, 2, 2) as input. Expected: true, Result: ${p2(1, 2, 2)}")
+  println(s"Test p2 function passing (2, 2, 3) as input. Expected: false, Result: ${p2(2, 2, 3)}")
+
+  println(s"Test p3 function passing (1, 2, 2) as input. Expected: true, Result: ${p3(1)(2)(2)}")
+  println(s"Test p3 function passing (2, 1, 1) as input. Expected: false, Result: ${p3(2)(1)(1)}")
+
+  println(s"Test p4 function passing (1, 2, 2) as input. Expected: true, Result: ${p4(1, 2, 2)}")
+  println(s"Test p4 function passing (3, 2, 2) as input. Expected: false, Result: ${p4(3, 2, 2)}")
+
+
+/*println("Task 5")
+println("Task 6")
+println("Task 7")
+println("Task 8")
+println("Task 9")*/
+
+
 
 
